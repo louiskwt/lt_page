2e3fce1e938198a6a3cf7c5f19555671a61b2c22
Louis Tsang <kawingt2@gmail.com>
2023-10-08 21:19:56 +0800

    set up detailed view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Post
 
@@ -7,3 +7,7 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
