48f8f0c3b9e22f0342ed9711576ad56f264fcc10
Louis Tsang <kawingt2@gmail.com>
2023-10-10 23:53:00 +0800

    added created at and formatted date

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title[:25]
