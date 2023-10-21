b8e408534407d03adcc555a72e4ed9e781f93493
Louis Tsang <kawingt2@gmail.com>
2023-10-21 23:57:38 +0800

    expanded blog model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Post(models.Model):
     title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256)
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-published_at']
+        indexes = [
+            models.Index(fields=['-published_at'])
+        ]
 
     def __str__(self):
         return self.title[:25]
