a84493db0de58c90bf3500be3d6e59d12d8ea857
Louis Tsang <kawingt2@gmail.com>
2023-10-22 09:15:41 +0800

    added status and run migrations

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,15 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     created_at = models.DateTimeField(auto_now_add=True)
     published_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
