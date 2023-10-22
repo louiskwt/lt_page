534c0f13da2033d8d9845300fb343369cd5bd863
Louis Tsang <kawingt2@gmail.com>
2023-10-22 09:36:03 +0800

    expanded admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'published_at', 'status']
+    list_filter = ['status', 'created_at', 'published_at']
+    search_fields = ['title', 'body']
+    raw_id_fields = ['author']
+    date_hierarchy = 'published_at'
+    ordering = ['status', 'published_at']
