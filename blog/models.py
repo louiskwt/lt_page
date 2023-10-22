11ab37cc7e47f22e18b7dd3273737c9c4e0662db
Louis Tsang <kawingt2@gmail.com>
2023-10-22 10:48:19 +0800

    updated canonical URL

--- blog/models.py
+++ blog/models.py
@@ -35,4 +35,4 @@ class Post(models.Model):
         return self.title[:25]
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs={"pk": self.pk})
+        return reverse("post_detail", args=[self.id])
