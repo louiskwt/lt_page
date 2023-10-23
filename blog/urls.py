9eac37fe867a7d6274dbaf6739cc0a7be0d9580b
Louis Tsang <kawingt2@gmail.com>
2023-10-23 14:07:04 +0800

    set up blog page template and urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("blogs", views.blog_list, name="blogs"),
     path("blogs/<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail"),
 ]
