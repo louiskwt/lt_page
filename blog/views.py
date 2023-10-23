7855cf1411c3324e9b64edf4210dab7cb7a667cb
Louis Tsang <kawingt2@gmail.com>
2023-10-23 23:55:35 +0800

    copy change and removed unused code

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
