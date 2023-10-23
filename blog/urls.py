94491957d16b8dee8ffe07a470ea341f73565edb
Louis Tsang <kawingt2@gmail.com>
2023-10-23 23:54:55 +0800

    implemented class based view

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.HomePageView.as_view(), name="home"),
     path("blogs", views.blog_list, name="blogs"),
     path("blogs/<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,17 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def home(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 5)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    
-    return render(request, 'home.html', {'post_list': posts})
+class HomePageView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'post_list'
+    paginate_by = 3
+    template_name = 'home.html'
+
 
 def blog_list(request):
     post_list = Post.published.all()
