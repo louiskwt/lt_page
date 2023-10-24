a7d6c816624f061d704c2f1503a6483f0122356c
Louis Tsang <kawingt2@gmail.com>
2023-10-24 23:41:01 +0800

    filtered by tag in blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from .models import Post
 
@@ -12,8 +13,13 @@ class HomePageView(ListView):
     template_name = 'home.html'
 
 
-def blog_list(request):
+def blog_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 5)
     page_number = request.GET.get('page', 1)
     try:
@@ -23,7 +29,7 @@ def blog_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blogs.html', {'post_list': posts }) 
+    return render(request, 'blogs.html', {'post_list': posts, 'tag': tag }) 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, published_at__year=year, published_at__month=month, published_at__day=day)
