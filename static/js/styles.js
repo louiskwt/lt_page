f55dcb1ee3775b4ecabf72b88cd1247ca314c050
Louis Tsang <kawingt2@gmail.com>
2023-10-28 00:07:54 +0800

    fixed markdown style with tailwind plugin

--- static/js/styles.js
+++ static/js/styles.js
@@ -1,15 +1,3 @@
 window.onload = () => {
-  const headings = document.querySelectorAll("h1");
-  console.log(headings);
-
-  const headingClass = ["text-2xl", "font-bold"];
-
-  headings.forEach((heading) => {
-    const {classList} = heading;
-    headingClass.forEach((className) => {
-      if (!classList.contains("text-3xl")) {
-        classList.add(className);
-      }
-    });
-  });
+  console.log("hello");
 };
